bc933690dc40cba9ec13d49c0fa93b995770bd4f
kiibabe <kisetsuirie@gmail.com>
2021-06-28 18:24:32 +0900

    Added a toolbar for Dashboard

--- app/src/main/java/com/example/timed/Dashboard.java
+++ app/src/main/java/com/example/timed/Dashboard.java
@@ -1,8 +1,8 @@
 package com.example.timed;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,6 +10,9 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     public static void getAppData(String[] args) {
--- app/src/main/java/com/example/timed/WeekView.java
+++ app/src/main/java/com/example/timed/WeekView.java
@@ -1,6 +1,32 @@
 package com.example.timed;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class WeekView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+
+        return super.onSupportNavigateUp();
+    }
 }
